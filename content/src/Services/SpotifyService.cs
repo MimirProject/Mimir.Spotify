83b990fc78a1cd507af1b87092afbe53e2f2d1aa
lfmundim <lucasfmundim@ufmg.br>
2018-08-30 22:43:00 -0300

    Added auth key prop

--- content/src/Services/SpotifyService.cs
+++ content/src/Services/SpotifyService.cs
@@ -6,10 +6,12 @@ namespace Mimir.Spotify.Services
     public class SpotifyService : ISpotifyService
     {
         private readonly ISpotifyAPI _client;
+        private string _authorizationKey;
 
         public SpotifyService(string token)
         {
             _client = RestClient.For<ISpotifyAPI>("https://api.spotify.com/v1");
+            _authorizationKey = token;
         }
     }
 }
